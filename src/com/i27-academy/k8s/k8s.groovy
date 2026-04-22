7aaea1e282233d6cd44fda8f43ef845a3af106b3
hari <rakesh@gmail.com>
2026-04-22 22:24:42 +0530

    added shared lib

---
+++ src/com/i27-academy/k8s/k8s.groovy
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// src/com/i27-academy/
+// ├── builds/
+// │   └── builds.groovy       ← Docker/Build methods only for ci 
+// └── k8s/
+//     └── k8s.groovy          ← Kubernetes methods used for cd
+
+// vars/
+// └── dockerpipeline.groovy   ← Main pipeline (entry point)
+
+
+
