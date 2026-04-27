ffadc346ce8853a471f168c3a41d886e825e84ab
hari <rakesh@gmail.com>
2026-04-27 23:51:50 +0530

    df

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -1,7 +1,6 @@
-
 package com.i27academy.k8s
 
-class K8s {
+class K8s implements Serializable {
 
     def jenkins
 
@@ -9,69 +8,11 @@ class K8s {
         this.jenkins = jenkins
     }
 
-}
-  // implemnet the logic to connect the k8s cluster 
-    def authlogin(ClusterName, Zone, ProjectID) {
+    def authlogin(clusterName, clusterZone, projectId) {
         jenkins.sh """
-        echo "Authenticating to GKE cluster..."
-        gcloud container clusters get-credentials ${ClusterName} --zone ${Zone} --project ${ProjectID}
-        kubectl get nodes
+            gcloud container clusters get-credentials ${clusterName} --zone ${clusterZone} --project ${projectId}
+            kubectl get nodes
         """
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// src/com/i27-academy/
-// ├── builds/
-// │   └── builds.groovy       ← Docker/Build methods only for ci 
-// └── k8s/
-//     └── k8s.groovy          ← Kubernetes methods used for cd
-
-// vars/
-// └── dockerpipeline.groovy   ← Main pipeline (entry point)
-
-
-
+}
