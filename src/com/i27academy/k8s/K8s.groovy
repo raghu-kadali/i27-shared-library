c2c551be012b3f00fc7605f3de4039ec715782c0
hari <rakesh@gmail.com>
2026-04-30 00:19:51 +0530

    smll changes in sed area

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -19,12 +19,8 @@ class K8s implements Serializable {
 // create a method to deploy k8s applications
 def k8sdeploy(filename,docker_image,namespace) {
     jenkins.sh """
-        echo "Current dir: \$(pwd)"
-        ls -la
-        ls -la .cicd/
-
         echo "deploying into gke cluster"
-        sed -i |DIT| ${docker_image}|g" .cicd/${filename}
+        sed -i "s|DIT|${docker_image}|g" .cicd/${filename}
         kubectl apply -f .cicd/k8s_dev.yaml -n ${namespace}
     """
 }
